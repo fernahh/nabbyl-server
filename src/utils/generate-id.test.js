8fc2cb1e6083f5d4ae851c9e5ffecccfb81ea6ba
Luiz Fernando Rodrigues <fernahh@gmail.com>
2018-12-20 17:45:04 -0200

    Create generate id util function

---
+++ src/utils/generate-id.test.js
@@ -0,0 +1,17 @@
+const generateID = require('./generate-id')
+const nanoid = require('nanoid')
+
+jest.mock('nanoid')
+
+describe('Generate ID', () => {
+  const mockNanoid = () => {
+    nanoid.mockImplementation(() => 'abcdef')
+  }
+
+  it('get some random value', () => {
+    mockNanoid();
+    const id = generateID()
+    expect(id).toEqual('abcdef')
+    expect(nanoid).toBeCalled()
+  })
+})
