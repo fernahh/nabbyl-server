590f8b7a77187a36cbe0d7cc248de93302189483
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-02-10 18:17:19 -0200

    Create handler for me endpoint

---
+++ src/me/handlers/get/get-me-handler.js
@@ -0,0 +1,18 @@
+const { get } = require('../../../utils/http')
+const { spotify } = require('../../../config/environment')
+
+module.exports = (req, res) => {
+  const url = `${spotify.apiUrl}/me/`
+
+  get(url, buildHeaders(req)).then(response => {
+    res.json(response.data)
+  })
+}
+
+function buildHeaders(request) {
+  return {
+    headers: {
+      'Authorization': request.headers['authorization']
+    }
+  }
+}
---
+++ src/me/handlers/get/get-me-handler.test.js
@@ -0,0 +1,51 @@
+const getMeHandler = require('./get-me-handler')
+const { get } = require('../../../utils/http')
+const { spotify } = require('../../../config/environment')
+const promiseMock = require('@mocks/promise-mock')
+const responseMock = require('@mocks/response-mock')
+const requestMock = require('@mocks/request-mock')
+
+jest.mock('../../../utils/http')
+jest.mock('../../../config/environment', () =>
+  require('@mocks/environment-mock')
+)
+
+describe('Get Me Handler', () => {
+  const meResponseMock = {
+    data: {
+      foo: 'bar'
+    }
+  }
+
+  const stubGet = (type, response) => {
+    const promise = promiseMock(type, response)
+    get.mockReturnValue(promise)
+  }
+
+  const stubGetMeHandler = (request, body) => {
+    getMeHandler(request, body)
+  }
+
+  const mockRequest = () => {
+    requestMock.headers = {
+      authorization: 'Bearer some-token'
+    }
+  }
+
+  beforeEach(() => {
+    mockRequest()
+    stubGet('success', meResponseMock)
+    stubGetMeHandler(requestMock, responseMock)
+  })
+
+  it('send the me response', () => {
+    const url = `${spotify.apiUrl}/me/`
+    const config = {
+      headers: {
+        'Authorization': requestMock.headers['authorization']
+      }
+    }
+    expect(get).toBeCalledWith(url, config)
+    expect(responseMock.json).toBeCalledWith(meResponseMock.data)
+  })
+})
