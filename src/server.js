fe19e46c177e85a932139420247f174aff6b2763
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-04-12 15:43:55 -0300

    Configure deploy - closes #17

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 const { port, webClient } = require('../config/environment.js')
-const express = require('express')
+const app = require('express')()
 const cors = require('cors')
 const authRouter = require('./auth/router/auth-router')
 const albumsRouter = require('./albums/router/albums-router')
 const meRouter = require('./me/router/me-router')
 
-const app = express()
-
 app.use(cors({ origin: webClient.url, exposedHeaders: ['Offset'] }))
 app.use('/albums', albumsRouter)
 app.use('/auth', authRouter)
 app.use('/me', meRouter)
+
 app.listen(port)
